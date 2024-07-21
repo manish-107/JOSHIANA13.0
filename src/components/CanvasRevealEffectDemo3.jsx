3403e839d4d8c180c76bb0c3e812a900dd4ddb33
manish-107 <creativecrew2676@gmail.com>
2024-07-21 23:50:06 +0530

    landing card

--- src/components/CanvasRevealEffectDemo3.jsx
+++ src/components/CanvasRevealEffectDemo3.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { CanvasRevealEffect } from "../components/ui/canvas-reveal-effect";
 import postimg from "../assets/imghead.jpg";
+import CardGrid from "./LandingCard";
 
 export default function CanvasRevealEffectDemo3() {
   const [hovered, setHovered] = React.useState(false);
@@ -10,7 +11,7 @@ export default function CanvasRevealEffectDemo3() {
     <div
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
-      className=" flex flex-col lg:flex-row items-center justify-center p-5 border-8 border-black bg-black w-full gap-4 mx-auto px-8 rounded-3xl"
+      className=" p-5 border-8 border-black bg-black w-full gap-4 mx-auto px-8 rounded-3xl"
     >
       {/* Animation Effect */}
       <AnimatePresence>
@@ -36,59 +37,66 @@ export default function CanvasRevealEffectDemo3() {
       </AnimatePresence>
 
       {/* Content Section */}
-      <div className="flex flex-col lg:flex-row w-full z-20 mt-10 m-5 md:m-32 md:mt-0 gap-4 pt-0 md:pt-32 lg:gap-8 ">
-        {/* Image Section */}
-        <div
-          className=" flex justify-center items-end wow fadeIn"
-          data-wow-delay="0.1s"
-          style={{
-            visibility: "visible",
-            animationDelay: "0.1s",
-            animationName: "fadeIn",
-          }}
-        >
-          <img
-            className="w-56 h-auto object-cover rounded-lg shadow-md"
-            src={postimg}
-            alt="Joshiana Event"
-          />
-        </div>
+      <div className="flex flex-col lg:flex-row items-center justify-center">
+        <div className=" flex flex-col lg:flex-row w-full z-20  mt-10 m-5 md:m-32 md:mt-0 gap-4 pt-0 md:pt-32 lg:gap-8">
+          {/* Image Section */}
+          <div
+            className="flex justify-center items-center wow fadeIn"
+            data-wow-delay="0.1s"
+            style={{
+              visibility: "visible",
+              animationDelay: "0.1s",
+              animationName: "fadeIn",
+            }}
+          >
+            <img
+              className="w-56 h-auto object-cover rounded-lg shadow-md"
+              src={postimg}
+              alt="Joshiana Event"
+            />
+          </div>
 
-        {/* Text Section */}
-        <div
-          className="flex-1 flex flex-col  lg:text-left wow fadeIn"
-          data-wow-delay="0.5s"
-          style={{
-            visibility: "visible",
-            animationDelay: "0.5s",
-            animationName: "fadeIn",
-          }}
-        >
-          <h1 className="text-uppercase mb-4 text-3xl pt-5 font-bold text-white">
-            Joshiana 13.0 | 2022
-          </h1>
-          <p className="text-white text-justify">
-            Joshiana 13.0 is a National level IT fest organized by the
-            Department of Computer Applications in association with TECHNOCRATS,
-            the student Association of MCA of St. Joseph Engineering College,
-            Mangaluru. As the developers in a world of fast-growing technology,
-            the productivities and energies embodied in the urban culture are
-            also changing. So, we welcome you to the theme of Joshiana 11.0 -
-            THE URBAN MATRIX which means - ‘a simulation in our urban
-            environment’. So come take a walk through our URBAN MATRIX and
-            experience the simulation on the 6th and 7th of September 2022 at{" "}
-            <a
-              href="https://www.sjec.ac.in/"
-              className="text-blue-400 hover:underline"
-            >
-              St. Joseph Engineering College
-            </a>
-            .
-          </p>
+          {/* Text Section */}
+          <div
+            className="flex-1 flex flex-col lg:text-left wow fadeIn"
+            data-wow-delay="0.5s"
+            style={{
+              visibility: "visible",
+              animationDelay: "0.5s",
+              animationName: "fadeIn",
+            }}
+          >
+            <h1 className="text-uppercase mb-4 text-3xl pt-5 font-bold text-white">
+              Joshiana 13.0 | 2022
+            </h1>
+            <p className="text-white text-justify">
+              Joshiana 13.0 is a National level IT fest organized by the
+              Department of Computer Applications in association with
+              TECHNOCRATS, the student Association of MCA of St. Joseph
+              Engineering College, Mangaluru. As the developers in a world of
+              fast-growing technology, the productivities and energies embodied
+              in the urban culture are also changing. So, we welcome you to the
+              theme of Joshiana 11.0 - THE URBAN MATRIX which means - ‘a
+              simulation in our urban environment’. So come take a walk through
+              our URBAN MATRIX and experience the simulation on the 6th and 7th
+              of September 2022 at{" "}
+              <a
+                href="https://www.sjec.ac.in/"
+                className="text-blue-400 hover:underline"
+              >
+                St. Joseph Engineering College
+              </a>
+              .
+            </p>
+          </div>
         </div>
       </div>
+      {/* Card Grid Section */}
 
       <div className="absolute inset-0 rounded-xl [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
+      <div className="relative flex flex-col items-center w-full z-20 mt-10 mb-5 md:mb-32">
+        <CardGrid />
+      </div>
     </div>
   );
 }
