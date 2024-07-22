80f462252d4abaf52978fbe0b89fa917d4c00643
manish-107 <creativecrew2676@gmail.com>
2024-07-23 01:10:55 +0530

    Update project and prepare for redeployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "JOSHIANA13.0",
+  base: "/JOSHIANA13.0/",
   build: {
     outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Define manual chunks for libraries or large modules to split code better
+          vendor: ["react", "react-dom"], // Example for React libraries
+          // Add other chunks here if necessary
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Increase the chunk size warning limit if needed
   },
 });
